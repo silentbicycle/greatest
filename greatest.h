d78e46366698d6b471ddc0616d03459b47ba7020
Scott Vokes <vokes.s@gmail.com>
2014-10-04 17:02:39 -0400

    Update year on copyright.

--- greatest.h
+++ greatest.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011 Scott Vokes <vokes.s@gmail.com>
+ * Copyright (c) 2011-2014 Scott Vokes <vokes.s@gmail.com>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
