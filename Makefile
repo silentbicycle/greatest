silentbicycle/greatest

commit 9a93b49ce4c3c3ee1be7009365da294c6f0608e0

Makefile: All programs should depend on the Makefile.

Makefile
@@ -36,6 +36,9 @@ example_cpp: example_cpp.cpp
 *.o: Makefile
 *.o: greatest.h
 
+${PROGRAMS_C}: greatest.h
+${PROGRAMS_CPP}: greatest.h
+
 clean:
 	rm -f ${PROGRAMS_C} ${PROGRAMS_CPP} *.o *.core
 

commit 2fbe39a3d5b16f802a70a484ce61acb5d8e0ab2a

Makefile: Fix dependency broken in 9a93b49.
That change added extra dependencies that broke incremental
rebuilds -- use $< to note that the .o files should only directly
use the .c file when rebuilding, but be invalidated by changes to
greatest.h or the Makefile.

Makefile
@@ -28,17 +28,22 @@ all: all_c
 all_c: ${PROGRAMS_C}
 all_cpp: ${PROGRAMS_CPP}
 
-example: example.c greatest.h example-suite.o
-	${CC} -o $@ example.c example-suite.o ${CFLAGS} ${LDFLAGS}
+example: example.o example-suite.o
+example_no_suite: example_no_suite.o
+example_no_runner: example_no_runner.o
+example_random: example_random.o
 
 example_cpp: example_cpp.cpp
 	${CXX} -o $@ example_cpp.cpp ${CPPFLAGS} ${LDFLAGS}
 
+%.o: %.c
+	${CC} -c -o $@ ${CFLAGS} $<
+
+%: %.o
+	${CC} -o $@ ${LDFLAGS} $^
+
 *.o: Makefile
 *.o: greatest.h
-
-${PROGRAMS_C}: greatest.h
-${PROGRAMS_CPP}: greatest.h
 
 clean:
 	rm -f ${PROGRAMS_C} ${PROGRAMS_CPP} *.o *.core